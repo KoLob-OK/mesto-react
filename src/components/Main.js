56cea9a8f625f0693a21d046ed1a47ef39cda60a
KoLobOK <webkolobok@mail.ru>
2022-10-21 16:03:27 +0300

    features: get user data via api request

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,30 +1,52 @@
 import React from 'react';
 import userDefaultAvatar from "../images/user-avatar.png";
+import api from "../utils/api";
 // import {selectors} from "../utils/constants";
 
 function Main(props) {
+    const [userAvatar, setUserAvatar] = React.useState(userDefaultAvatar);
+    const [userName, setUserName] = React.useState('');
+    const [userDescription, setUserDescription] = React.useState('');
+
+    React.useEffect(() => {
+        api.getUserData()
+            .then((data) => {
+                setUserAvatar(data.avatar);
+                setUserName(data.name);
+                setUserDescription(data.about);
+            })
+            .catch((err) => {
+                console.log(`Ошибка: ${err}`);
+            });
+    }, []);
+
     return (
         <main className="content page__content">
 
             <section className="profile content__profile">
                 <div className="profile__card">
-                    <img className="profile__avatar"
-                         src={userDefaultAvatar}
-                         alt="Ваш аватар"
-                    />
+
+                    <div className="profile__avatar">
+                        <div className="profile__avatar-img"
+                             style={{backgroundImage: `url(${userAvatar})`}}>
+                        </div>
+                    </div>
+
                     <button className="profile__avatar-button"
                             type="button"
                             aria-label="Обновить аватар"
                             onClick={props.onEditAvatar}>
                     </button>
+
+
                     <div className="profile__info">
-                        <h1 className="profile__title">Жак-Ив</h1>
+                        <h1 className="profile__title">{userName}</h1>
                         <button className="profile__edit-button"
                                 type="button"
                                 aria-label="Редактировать профиль"
                                 onClick={props.onEditProfile}>
                         </button>
-                        <p className="profile__description">Исследователь</p>
+                        <p className="profile__description">{userDescription}</p>
                     </div>
 
                 </div>
